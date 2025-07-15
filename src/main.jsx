ee43ed6209ca49a93ecd1eb54fd0978b90389d5c
peszabolcs <perjesidev@gmail.com>
2025-07-15 17:51:35 +0200

    feat: Implement email confirmation and error templates with styling

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (window.location.hash.includes('access_token=') && window.location.hash.includes('type=signup')) {
+    window.location.href = 'https://parksafe.hu/success';
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
