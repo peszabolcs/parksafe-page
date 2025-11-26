4ae900e4c610c251e0937d264ff89889791c9096
peszabolcs <perjesidev@gmail.com>
2025-11-26 14:32:16 +0100

    update colors on hero

--- src/components/ui/aurora-background.tsx
+++ src/components/ui/aurora-background.tsx
@@ -16,7 +16,7 @@ export const AuroraBackground = ({
     <main>
       <div
         className={cn(
-          "relative flex flex-col h-[100vh] items-center justify-center bg-zinc-50 dark:bg-zinc-900 text-slate-950 dark:text-white transition-bg",
+          "relative flex flex-col h-[100vh] items-center justify-center bg-zinc-50 dark:bg-black text-slate-950 dark:text-white transition-bg",
           className
         )}
         {...props}
@@ -38,7 +38,7 @@ export const AuroraBackground = ({
             after:[background-size:200%,_100%]
             after:animate-aurora after:[background-attachment:fixed] after:mix-blend-difference
             pointer-events-none
-            absolute -inset-[10px] opacity-50 will-change-transform`,
+            absolute -inset-[10px] opacity-30 will-change-transform`,
 
               showRadialGradient &&
                 `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]`
